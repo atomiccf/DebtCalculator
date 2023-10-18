e0aefde12eba48772fee7390f1241731d9d3ba7f
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2023-10-18 11:28:09 +0200

    add getCurrentDate function
    refactor getRate function

--- script2.js
+++ script2.js
@@ -6,25 +6,58 @@ const debt = document.getElementsByClassName('debt')
 const outOne = document.getElementsByClassName('out1')
 const outTwo = document.getElementsByClassName('out2')
 const outThree = document.getElementsByClassName('out3')
-let rate = 0
+let ratemain = 0
+let currentDate = getCurrentDate();
+const url = `https://www.nbrb.by/api/refinancingrate?ondate=${currentDate}`;
+
+/*get current date*/
+
+function getCurrentDate () {
+
+    const currentDate = new Date().toLocaleDateString().split('/')
+
+    for (let i=0; i === 0; i++){
+
+        let temp;
+
+        if(currentDate[0]) {
+
+            temp = currentDate[0];
+            currentDate[0] = currentDate[i+1];
+            currentDate[i+1] = temp;
+
+        } else {
+
+            continue
+        }
+
+        return currentDate.reverse().join('-')
+
+    }
+}
+
+/*calculate payment delay*/
 
 function calculateDade(start, end) {
     let dateArr = []
 
     for (let i = 0; i < dateStart.length && i < dateEnd.length; i++) {
+
         const a = new Date(start[i].value);
         const b = new Date(end[i].value);
+
         const day = Math.ceil(86400000 / (1000 * 3600 * 24));
         let timeDiff = Math.abs(b.getTime() - a.getTime());
         let dayDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
         dateArr.push(dayDiff + day)
-        /*(isChecked()) ?  console.log([dayDiff + day]): console.log([dayDiff])*/
 
     }
 
     return dateArr
 
 }
+
+/*calculate fine*/
 
 function calculateFine(debt) {
     let arrFine = [];
@@ -38,6 +71,7 @@ function calculateFine(debt) {
     return arrFine;
 }
 
+/*calculate debt percents*/
 
 function calculatePercents(debt) {
     let arrPercent = [];
@@ -45,7 +79,7 @@ function calculatePercents(debt) {
 
     for (let j = 0; j < debt.length; j++) {
 
-        arrPercent.push((debt[j].value / 100 * rate * arrDelay[j] / 365).toFixed(2));
+        arrPercent.push((debt[j].value / 100 * ratemain * arrDelay[j] / 365).toFixed(2));
 
     }
 
@@ -53,6 +87,8 @@ function calculatePercents(debt) {
 
 
 }
+
+/*render result*/
 
 function render(delay, fine, percent) {
     let arrDelay = calculateDade(dateStart, dateEnd);
@@ -72,42 +108,34 @@ function render(delay, fine, percent) {
     })
 }
 
+
+
+
+/*get rate */
+
+async function getRate  (){
+
+  const response = await fetch(url)
+
+   return await response.json()
+
+}
+getRate().then( rate=>{
+
+    ratemain = rate[0].Value
+
+
+})
+
+/*event listeners*/
+
 start.addEventListener('click', () => {
 
     render(outOne, outTwo, outThree)
 
 });
 
-
-
 del.addEventListener('click', () => {
 
     app.lastChild.remove()
 });
-
-const url = 'https://www.nbrb.by/api/refinancingrate';
- fetch(url)
-    .then ( (response) => {
-        if (response.status !== 200) {
-
-            console.log('Something going wrong' + response.status);
-            return
-        }
-
-        return response.json()
-
-    })
-
-    .then (data => {
-
-        let arrayData = data
-
-        rate = arrayData[arrayData.length-1].Value
-        return rate
-
-    })
-    .catch ( err =>{
-
-        console.log('Fetch error',err);
-
-    })
